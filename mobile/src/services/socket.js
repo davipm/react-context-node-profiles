d550a355f9a058ed3da8021bde88980e99cd2546
davi <davi.p.m94@gmail.com>
2020-06-17 18:08:05 -0300

    refactoring

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,6 +1,6 @@
 import socketio from "socket.io-client";
 
-const socket = socketio("http://192.168.0.101:3333", {
+const socket = socketio("http://192.168.0.100:3333", {
   autoConnect: false,
 });
 
--- webapp/client/src/hooks/usePosition.js
+++ webapp/client/src/hooks/usePosition.js
@@ -1,13 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function usePosition() {
   const [position, setPosition] = useState({});
 
   useEffect(() => {
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 3000 });
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+        timeout: 3000,
+      });
     } else {
-      alert('You browser dont support geolocation');
+      alert("You browser dont support geolocation");
     }
   }, []);
 
